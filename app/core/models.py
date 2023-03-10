5ff151864a35cece8cff4cd8b8e4134321f39be2
Btslade <btslade97@gmail.com>
2023-03-10 11:54:25 -0500

    Implement User Model
        Migration contains Django's generated code based on our User Class

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
-from django.db import models  # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+
+        # self.model is the same is creating a new user
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)  # supports adding multiple databases
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()   # Assigns UserManager to this class
+
+    USERNAME_FIELD = 'email'  # Must be defined or else attribute error occurs
