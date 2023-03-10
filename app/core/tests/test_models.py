cedbfd9e5bc1abf68d2adc3d9bde42e407235a49
Btslade <btslade97@gmail.com>
2023-03-10 13:09:29 -0500

    User's must have an email address

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,11 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'smaple123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """User must have an email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email='',
+                password='test123'
+            )
