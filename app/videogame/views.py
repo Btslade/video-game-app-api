ff30e17649c66f2842859cf9e84f60e4507b65bf
Btslade <btslade97@gmail.com>
2023-06-01 20:07:23 -0400

    Refactored Viewsets
    Created BaseVideogameAttrViewset that handles
        mixins
        authentication_classes
        permission_classes
        getting a queryset

--- app/videogame/views.py
+++ app/videogame/views.py
@@ -41,31 +41,26 @@ class VideogameViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.DestroyModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.ListModelMixin,
-                 viewsets.GenericViewSet):
+class BaseVideogameAttrViewSet(mixins.DestroyModelMixin,
+                               mixins.UpdateModelMixin,
+                               mixins.ListModelMixin,
+                               viewsets.GenericViewSet):
+    """Base viewset for videogame attributes."""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BaseVideogameAttrViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """Filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class ConsoleViewSet(mixins.DestroyModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.ListModelMixin,
-                     viewsets.GenericViewSet):
+class ConsoleViewSet(BaseVideogameAttrViewSet):
     """Manage consoles in the database."""
     serializer_class = serializers.ConsoleSerializer
     queryset = Console.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """Filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
